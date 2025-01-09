a0aba3b071a1f6600ee9ddcdd6b48e83630ded3e
Taisei <taisei.960420@gmail.com>
2025-01-10 06:06:55 +0900

    スタイル適用

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { WorkoutFormComponent } from './components/workout-form/workout-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WorkoutFormComponent],
+  imports: [RouterOutlet, WorkoutFormComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
