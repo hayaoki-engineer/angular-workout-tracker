06ffd6b431c7c3b82628ed1ba65c0bdcb4594481
Taisei <taisei.960420@gmail.com>
2025-01-09 09:11:43 +0900

    ルーティング設定を定義

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { WorkoutFormComponent } from './components/workout-form/workout-form.component';
+import { WorkoutListComponent } from './components/workout-list/workout-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+  { path: 'new', component: WorkoutFormComponent },
+  { path: 'list', component: WorkoutListComponent },
+];
