bd912f87e56f6290ee7ad28b7fc2b3fbd389cd13
Taisei <taisei.960420@gmail.com>
2025-01-11 15:39:22 +0900

    データ構造を定義

---
+++ src/app/model/workout.model.ts
@@ -0,0 +1,18 @@
+// セット
+export interface Set {
+  weight: number;
+  reps: number;
+}
+
+// 種目
+export interface Exercise {
+  name: string;
+  sets: Set[];
+}
+
+// ワークアウト
+export interface Workout {
+  id: string;
+  date: string;
+  exercises: Exercise[];
+}
