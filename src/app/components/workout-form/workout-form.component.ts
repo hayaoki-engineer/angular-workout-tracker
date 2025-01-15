8eb869f65b71d223877d666f5cb0c6627700f294
Taisei <taisei.960420@gmail.com>
2025-01-16 06:57:17 +0900

    セット追加の関数を実装

--- src/app/components/workout-form/workout-form.component.ts
+++ src/app/components/workout-form/workout-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Exercise } from '../../model/workout.model';
+import { WorkoutService } from '../../services/workout.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-workout-form',
@@ -8,5 +11,19 @@ import { Component } from '@angular/core';
   styleUrl: './workout-form.component.css'
 })
 export class WorkoutFormComponent {
+
+  exercises: Exercise[] = [
+    { name: '', sets: [{ weight: 0, reps: 0 }] }
+  ];
+
+  constructor(
+    private workoutService: WorkoutService,
+    private router: Router
+  ) { }
+  
+  // セットを追加
+  addSet(exerciseIndex: number) {
+    this.exercises[exerciseIndex].sets.push({ weight: 0, reps: 0 });
+  }
 
 }
