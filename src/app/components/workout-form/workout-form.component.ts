7e50dd86a0b73e62705f0670964861010260bce8
Taisei <taisei.960420@gmail.com>
2025-01-17 09:58:49 +0900

    種目を追加の関数を実装

--- src/app/components/workout-form/workout-form.component.ts
+++ src/app/components/workout-form/workout-form.component.ts
@@ -2,31 +2,33 @@ import { Component } from '@angular/core';
 import { Exercise, Workout } from '../../model/workout.model';
 import { WorkoutService } from '../../services/workout.service';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-workout-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './workout-form.component.html',
-  styleUrl: './workout-form.component.css'
+  styleUrl: './workout-form.component.css',
 })
 export class WorkoutFormComponent {
+  exercises: Exercise[] = [{ name: '', sets: [{ weight: 0, reps: 0 }] }];
 
-  exercises: Exercise[] = [
-    { name: '', sets: [{ weight: 0, reps: 0 }] }
-  ];
+  constructor(private workoutService: WorkoutService, private router: Router) {}
 
-  constructor(
-    private workoutService: WorkoutService,
-    private router: Router
-  ) { }
-  
   // セットを追加
   addSet(exerciseIndex: number) {
     this.exercises[exerciseIndex].sets.push({ weight: 0, reps: 0 });
   }
 
-  // 種目を削除 
+  // 種目を追加
+  addExercise() {
+    this.exercises.push({
+      name: '',
+      sets: [{ weight: 0, reps: 0 }],
+    });
+  }
+  // 種目を削除
   removeExercise(index: number) {
     if (this.exercises.length > 1) {
       this.exercises.splice(index, 1);
@@ -35,7 +37,7 @@ export class WorkoutFormComponent {
 
   // ワークアウトを追加
   onSubmit() {
-    if (this.exercises.some(e => e.name && e.sets.length > 0)) {
+    if (this.exercises.some((e) => e.name && e.sets.length > 0)) {
       // 今日の日付を取得
       const today = new Date();
       // 今日の日付を0時0分0秒に設定
@@ -45,7 +47,7 @@ export class WorkoutFormComponent {
       const workout: Workout = {
         id: Date.now().toString(),
         exercises: [...this.exercises],
-        date: today
+        date: today,
       };
 
       // ワークアウトを追加
@@ -55,7 +57,4 @@ export class WorkoutFormComponent {
       this.router.navigate(['/list']);
     }
   }
-
-
-
 }
